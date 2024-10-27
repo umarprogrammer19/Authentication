1d5d5f33c0c2cf4d1f8593ddaf387b2047167502
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-27 23:54:44 +0500

    Created Card

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,20 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Login() {
     return (
-        <h1>Login Page</h1>
+        <div className="flex justify-center items-center h-dvh">
+            <Card>
+                <CardHeader>
+                    <CardTitle>Card Title</CardTitle>
+                    <CardDescription>Card Description</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Card Footer</p>
+                </CardFooter>
+            </Card>
+        </div>
     );
 };
