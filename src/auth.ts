6e9c7526f6979d2bdd2c8b6d21f7a0c5ccb00561
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-27 20:51:58 +0500

    Added Authorize

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth, { CredentialsSignin } from "next-auth";
 import CredentialProvider from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
@@ -9,7 +9,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                 email: { label: "Email", type: "email" },
                 password: { label: "Password", type: "password" },
             },
+            authorize: async ({ email, password }) => {
+                if (typeof email !== "string") throw new CredentialsSignin({
+                    cause: "Email Do Not Match",
+                });
 
+                const user = { email, password };
+                if (password !== "passcode") throw new CredentialsSignin({
+                    cause: "Password Does Not Match",
+                });
+                else return user;
+            },
         })
     ]
 }) 
