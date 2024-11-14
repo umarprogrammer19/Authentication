7558383779aab95ae986e32b54ab31a294cb6565
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-11-14 14:57:57 +0500

    Login Done

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,18 +1,24 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Login() {
     return (
         <div className="flex justify-center items-center h-dvh">
             <Card>
                 <CardHeader>
-                    <CardTitle>Card Title</CardTitle>
-                    <CardDescription>Card Description</CardDescription>
+                    <CardTitle>Login</CardTitle>
                 </CardHeader>
-                <CardContent>
-                    <p>Card Content</p>
+                <CardContent >
+                    <form className="flex flex-col gap-4">
+                        <Input type="email" placeholder="Email" />
+                        <Input type="password" placeholder="Password" />
+                        <Button type="submit">Login</Button>
+                    </form>
                 </CardContent>
                 <CardFooter>
-                    <p>Card Footer</p>
+                    <Link href="/">Dont have an account? Sign Up</Link>
                 </CardFooter>
             </Card>
         </div>
