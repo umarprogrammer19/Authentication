daba6e8c3230d267a5bed402c58f53b987d25a8b
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-11-14 15:47:10 +0500

    SignUp

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,9 +1,33 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { User } from "@/models/userModels";
+import { hash } from "bcryptjs";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function SignUp() {
+    const signUp = async (formData: FormData) => {
+        "use server";
+        const name = formData.get('name') as string | undefined;
+        const email = formData.get("email") as string | undefined;
+        const password = formData.get("password") as string | undefined;
+
+        if (!name || !email || !password) {
+            throw new Error("Please fill in all fields");
+        }
+        const user = await User.findOne({ email });
+        if (user) {
+            throw new Error("Email already in use");
+        }
+        const hashPassword = await hash(password, 10);
+        await User.create({
+            name,
+            email,
+            password: hashPassword,
+        });
+        redirect("/login");
+    }
     return (
         <div className="flex justify-center items-center h-dvh">
             <Card>
@@ -11,11 +35,7 @@ export default function SignUp() {
                     <CardTitle>Sign Up</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <form action={async (formData: FormData) => {
-                        const name = formData.get('name') as string | undefined;
-                        const email = formData.get("email") as string | undefined;
-                        const password = formData.get("password") as string | undefined;
-                    }} className="flex flex-col gap-4">
+                    <form action={signUp} className="flex flex-col gap-4">
                         <Input placeholder="Name" name="name" />
                         <Input type="email" placeholder="Email" name="email" />
                         <Input type="password" placeholder="Password" name="password" />
